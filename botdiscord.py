36914ad4c8bf1c819f4d312cf5744e0a29cb04eb
Geoffrey-code <62614992+Geoffrey-code@users.noreply.github.com>
2020-03-27 17:47:09 +0100

    Add files via upload

--- botdiscord.py
+++ botdiscord.py
@@ -13,11 +13,18 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == "!hello":
-        await message.channel.send("Bonjour Humain")
-    if message.content == "!carapuce":
-        await message.content.send == "Beurkkkk":
-    print(message.content)
+        member_name = message.author.name
+        guild_name = message.guild.name
+        new_msg = await message.channel.send("Bonjour + "   +   member_name     +   " et bienvenue sur "   +   guild_name)
+        await new_msg.pin()
 
+em = discord.Embed(title="titre", description="desc", color=0xFF0000, timestamp=message.created_at)
+em.add_field(name="Un field", value="Youpi", inline=True)
+em.add_field(name="Un autre field", value="o/", inline=True)
+em.add.field(name="Un field pas sur la même ligne", value="grâce au 'inline'", inline=False)
+em.set.author(name="Un gars super", icon_url=message.author.avatar_url)
+em.set.author(name="Sur un super serveur", icon_url=message.guild.icon_url)
+em.set_image(url=​"https://cdn.discordapp.com/attachments/" + ​"567630033246617621/581496185424969749/1733852_0.jpg"​)
+await message.channel.send(embed=em)
 
-
-client.run("NjU4NjU3MTczMTM1Njg3NzA5.Xn4czQ.rfBTqUZT9zIyBJ1A3dGQo3mkSLo")
+client.run("NjU4NjU3MTczMTM1Njg3NzA5.Xn4nGA.5yJkWLv7Y_-8ToqtUaL-bC5XJoI")
